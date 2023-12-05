b9758973c250946cf35dfb0eee449f139c3503b2
victor <victor.murithi@student.moringaschool.com>
2023-12-05 08:27:37 +0300

    completed assignment

--- lib/song.py
+++ lib/song.py
@@ -1,4 +1,44 @@
 from config import CONN, CURSOR
 
 class Song:
-    pass
+    def __init__(self, name, album):
+        self.name = name
+        self.album = album
+
+    @classmethod
+    def create_table(cls):
+        sql = '''
+        CREATE TABLE IF NOT EXISTS songs (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            album TEXT
+        )
+        '''
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    def save(self):
+        sql = """
+        INSERT INTO songs (name, album)
+        VALUES (?, ?)
+        """
+        CURSOR.execute(sql, (self.name, self.album))
+        CONN.commit()
+        self.id = CURSOR.lastrowid
+    @classmethod
+    def create(cls,name,album):
+        song = Song(name,album)
+        song.save()
+        return song
+
+# Create the table
+Song.create_table()
+
+# Create a Song instance and save it to the database
+hello = Song('Hello', "25")
+hello.save()
+
+songs = CURSOR.execute('SELECT * FROM songs')
+[row for row in songs]
+
+print(songs)
